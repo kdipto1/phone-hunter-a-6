63bb05a342a27a9ff31301659c79fd0c804c6a83
kdipto1 <kdipto@outlook.com>
2022-03-01 01:03:15 +0600

    api connected

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 const inputSearchButton = () => {
-  
+  const searchText = document.getElementById("search-field").value;
+  const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showSearchResult(data.data))
+    // .then(data => console.log(data));
+}
+
+const showSearchResult = (phones) => {
+  const parent = document.getElementById("search-results");
+  parent.innerHTML = "";
+  // console.log(phones);
+  const showTwenty = phones.slice(0, 20);
+  showTwenty.forEach(phone => {
+    console.log(phone);
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <img src="${phone.image}" alt="">
+    <h2>Brand: ${phone.brand}</h2>
+    <h3>Phone Name: ${phone.phone_name}</h3>
+    `;
+    parent.appendChild(div)
+  });
 }
